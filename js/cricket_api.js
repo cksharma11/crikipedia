e1f938b0b699dd4227aa1a948f248ef59d0c503a
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-18 12:45:13 +0530

    Added Environment variable for api key

--- js/cricket_api.js
+++ js/cricket_api.js
@@ -1,5 +1,5 @@
 const request = require("request");
-const CRICK_API_KEY = "62cC1Nr6dCZEEqAycpQks3S7D0O2";
+const CRICK_API_KEY = process.env.CRICK_API;
 
 const getPlayerDetails = function(player, response) {
   const playerIdUrl = `https://cricapi.com/api/playerFinder?apikey=${CRICK_API_KEY}&name=${player}`;
